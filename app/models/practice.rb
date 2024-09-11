aba27524a6ada2c0c3786ee0ffff16f657458021
Devin Jaggernauth <neexis@gmail.com>
2024-09-11 09:08:29 -0400

    add basic validation

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,4 @@
 class Practice < ApplicationRecord
+  validates :name, :street_address, :city, :phone, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
 end
