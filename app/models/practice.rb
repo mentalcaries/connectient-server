0c3626621d26ebeb160596947d67411fb3ace173
Devin Jaggernauth <neexis@gmail.com>
2024-10-04 06:04:10 -0400

    set up practice relationships

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,4 +1,7 @@
 class Practice < ApplicationRecord
+  has_many :appointments
+  belongs_to :user
+
   validates :name, presence: true, length: { minimum: 2, maximum: 100 }
   validates :street_address, :city, :phone, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
